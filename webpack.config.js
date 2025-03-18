c7f22943337be0d0179cce7fe6bc0c1da8f6f28f
swc <cannizzaro75@gmail.com>
2025-03-18 19:56:27 -0400

    some styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const postCSSplugins = [
+    require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
