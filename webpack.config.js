bb04f5a0fce96b0a8f01e36c4216a660b1b997b0
swc <cannizzaro75@gmail.com>
2025-03-20 15:09:55 -0400

    webpack watching html files now as well

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,13 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     devServer: {
+        watchFiles: ["app/**/*.html"],
         static: {
-            directory: path.join(__dirname, "app")
+            directory: path.join(__dirname, "app"),
+            watch: false
         },
         hot: true,
-        port: 3000,
-        liveReload: false
+        port: 3000
     },
     mode: 'development',
     module: {
