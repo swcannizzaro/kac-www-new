e052948f3465f8297843db00083da0f93b65005f
swc <cannizzaro75@gmail.com>
2025-03-30 22:36:53 -0400

    Adding some efficiency tweaks

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,28 +1,44 @@
+import { throttle } from "lodash";
+
 class RevealOnScroll {
     constructor() {
         this.itemsToReveal = document.querySelectorAll('.showcase__group');
         this.hideInitially();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
     }
 
     events() {
-        window.addEventListener('scroll', () => {
-            console.log('scrolling func ran');
-            this.itemsToReveal.forEach((el) => {
+        window.addEventListener('scroll', this.scrollThrottle);
+    }
+
+    calcCaller() {
+        console.log('scrolling func ran');
+        this.itemsToReveal.forEach((el) => {
+            if (el.isRevealed == false) {
                 this.calculateIfScrolledTo(el);
-            });
+            }
         });
     }
 
     calculateIfScrolledTo(el) {
+        console.log('element was calculated');
         let scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100;
         if (scrollPercent < 75) {
             el.classList.add('reveal-items--is-visible');
+            el.isRevealed = true;
+            if (el.isLastItem) {
+                window.removeEventListener('scroll', this.scrollThrottle);
+            }
         }
     }
 
     hideInitially() {
-        this.itemsToReveal.forEach((el) => el.classList.add('reveal-items'));
+        this.itemsToReveal.forEach((el) => {
+            el.classList.add('reveal-items');
+            el.isRevealed = false;
+        });
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
     }
 }
 
