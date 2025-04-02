4d5a5978cb87fa24bae6cb84b9deccc265e5994c
swc <cannizzaro75@gmail.com>
2025-04-02 16:43:58 -0400

    Adding a few more reveal-on-scroll sections

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 let mobileMenu = new MobileMenu();
-new RevealOnScroll(document.querySelectorAll('.showcase__group'), 67);
-new RevealOnScroll(document.querySelectorAll('.footer__content-crypto'), 75);
+new RevealOnScroll(document.querySelectorAll('.showcase__group'), 72);
+new RevealOnScroll(document.querySelectorAll('.collect__content-cta'), 63);
+new RevealOnScroll(document.querySelectorAll('.collect__content-quote-list'), 72);
+new RevealOnScroll(document.querySelectorAll('.footer__content-crypto'), 81);
 
 if(module.hot) {
     module.hot.accept();
